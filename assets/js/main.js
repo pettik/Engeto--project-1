de5ddef12cd5b6ba2fe745536712b58bc1d858ec
pettik <65282144+pettik@users.noreply.github.com>
2023-10-16 16:25:21 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-console.log('ahoj');
 
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
@@ -32,9 +31,41 @@ const linkAction = () => {
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header');
+
+    this.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header')
+}
+
+window.addEventListener('scroll',blurHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up');
 
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll',scrollUp);
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute('id');
+        const sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            sectionClass.classList.add('active-link');
+        }else{
+        sectionClass.classList.remove('active-link');
+    }
+    })
+}
+window.addEventListener('scroll',scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
